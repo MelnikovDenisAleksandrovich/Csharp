01b05c0e1a67af45642cb50d1903308ee87e94b1
MelnikovDenisAleksandrovich <140500231+MelnikovDenisAleksandrovich@users.noreply.github.com>
2023-08-10 10:41:14 +0300

    Домашняя работа 2

--- task10/Program.cs
+++ task10/Program.cs
@@ -4,3 +4,10 @@
 918 -> 1
 */
 Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number >= 100)
+{
+    number /= 10;
+}
+int result = number % 10;
+Console.WriteLine(result);
--- task15/Program.cs
+++ task15/Program.cs
@@ -3,3 +3,36 @@
 7 -> да
 1 -> нет
 */
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1)
+{
+    Console.Write("Понедельник-Идти на работу");
+}
+else if (number == 2)
+{
+    Console.Write("Вторник-Идти на работу");
+}
+else if (number == 3)
+{
+    Console.Write("Среда-Идти на работу");
+}
+else if (number == 4)
+{
+    Console.Write("Четверг-Идти на работу");
+}
+else if (number == 5)
+{
+    Console.Write("Пятница-Идти на работу");
+}
+else if (number == 6)
+{
+    Console.Write("Суббота-Выходной");
+}
+else if (number == 7)
+{
+    Console.Write("Воскресенье-Выходной");
+}
+else
+{
+    Console.Write("Такого дня нет");
+}
