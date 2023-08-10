b419f075b61133e9bbab3f479e9394d31db1bf79
MelnikovDenisAleksandrovich <140500231+MelnikovDenisAleksandrovich@users.noreply.github.com>
2023-08-10 10:45:36 +0300

    Домашняя работа 2

--- task13/Program.cs
+++ task13/Program.cs
@@ -4,33 +4,13 @@
 32679 -> 6
 */
 
-
-int Prompt(string msg)
+Console.Write("Напишите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
 {
-
-    System.Console.WriteLine($»{ msg}»);
-return Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(number % 10);
 }
-int number = Prompt(«Введите число»);
-
-int fnumber(int number)
-{
-    while (number > 999)
-    {
-        number /= 10;
-    }
-    return number % 10;
-}
-
-bool check(int number)
-{
-    if (number < 100)
-        return false;
-    else return true;
-}
-
-if (check(number) != true)
-    System.Console.WriteLine(«Третьей цифры нет»);
 else
-    System.Console.WriteLine($»Третья цифра числа { number}
-является {fnumber(number)}»);
+{
+    Console.Write("Число не трехзначное");
+}
