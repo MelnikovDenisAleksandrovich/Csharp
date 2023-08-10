f3fc5731c806732a58e004d7c2d7e3c4e0d93819
MelnikovDenisAleksandrovich <140500231+MelnikovDenisAleksandrovich@users.noreply.github.com>
2023-08-10 10:51:37 +0300

    Домашняя работа 2.

--- task13/Program.cs
+++ task13/Program.cs
@@ -4,13 +4,3 @@
 32679 -> 6
 */
 
-Console.Write("Напишите число ");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number > 99 && number < 1000)
-{
-    Console.WriteLine(number % 10);
-}
-else
-{
-    Console.Write("Число не трехзначное");
-}
