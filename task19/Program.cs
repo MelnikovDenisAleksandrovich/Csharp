1ff79eae013a5006c53f9f3ea7d9fcd5c9cd5632
unknown <melpostov@yandex.rugit config --global user.name>
2023-08-27 17:08:04 +0300

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+Console.WriteLine("Введите пятизначное число ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int numberTurn = 0;
+
+if (userNumber < 0)
+    userNumber = userNumber * -1;
+
+if (userNumber > 9999 && userNumber < 100000)
+{
+    for (int i = userNumber; i > 0; i /= 10)
+    {
+        numberTurn = i % 10 + numberTurn * 10;
+    }
+    if (userNumber == numberTurn)
+        Console.WriteLine("true");
+    else
+        Console.WriteLine("false");
+}
+else
+    Console.WriteLine("error input");
