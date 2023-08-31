b8c0eaf1741cf7cf02bc65fb72fc587dd0146309
unknown <melpostov@yandex.rugit config --global user.name>
2023-08-31 15:53:05 +0300

    task24

---
+++ task24/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая принимает на вход число (А)
+и выдает сумму чисел от 1 до А.*/
+
+Console.WriteLine("Введите число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+/*
+int sum = 0;
+for(int i = 1; i<= userNumber; i++)
+{
+    sum += i;
+}
+Console.WriteLine(sum);
+*/
+
+int SunOfNumber(int number)
+{
+    int sum = 0;
+for(int i = 1; i<= userNumber; i++)
+{
+    sum += i;
+}
+    return sum;
+}
+
+Console.WriteLine($"Сумма цифр в числе {userNumber} равна {SunOfNumber(userNumber)}");
