872961de3c32c9e23dbc2731f5e83b525e3e2588
unknown <melpostov@yandex.rugit config --global user.name>
2023-09-01 03:18:25 +0300

    task26

---
+++ task26/Program.cs
@@ -0,0 +1,21 @@
+﻿/*напишите программу,которая принимает на вход чило и выдает количество цифр в числе.
+
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+System.Console.WriteLine("Введите число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int Quantity(int number);
+{
+    int count = 0;
+    while(number > 0)
+    {
+        count++;
+        number = number / 10;
+    }
+    return count;
+}
+System.Console.WriteLine(Quantity(userNumber));
