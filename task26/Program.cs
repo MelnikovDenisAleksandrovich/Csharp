caceaa5c27511ee5e4602dc811d5c1eebccb8cbe
unknown <melpostov@yandex.rugit config --global user.name>
2023-09-01 16:07:51 +0300

    task26

--- task26/Program.cs
+++ task26/Program.cs
@@ -8,10 +8,10 @@
 System.Console.WriteLine("Введите число");
 int userNumber = Convert.ToInt32(Console.ReadLine());
 
-int Quantity(int number);
+int Quantity(int number)
 {
     int count = 0;
-    while(number > 0)
+    while( number > 0)
     {
         count++;
         number = number / 10;
