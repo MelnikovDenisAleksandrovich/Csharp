f745e67eb6db1aca4b551b8e8cb68cdad571dfeb
unknown <melpostov@yandex.rugit config --global user.name>
2023-09-01 16:41:03 +0300

    task28

---
+++ task28/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая принимает на вход число N и 
+выдает произвидение чисел от 1 до N.
+
+4 -> 24
+5 -> 120
+*/
+
+System.Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int ProductOfNumber(int number)
+{
+    int product = 1;
+for (int i = 1; i <= userNumber; i++)
+{
+    product *= i;
+}
+return product;
+}
+
+System.Console.WriteLine($"Произведение в числе {userNumber} равна {ProductOfNumber(userNumber)}");
