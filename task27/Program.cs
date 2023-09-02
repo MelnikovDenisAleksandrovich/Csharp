55fdd603b715b50cefad18159611340c0e414713
unknown <melpostov@yandex.rugit config --global user.name>
2023-09-02 09:23:45 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу,которая принимает на вход 
+число и выдает сумму цифр в числе 
+
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.WriteLine("Введите число");
+int userNumber =Convert.ToInt32(Console.ReadLine());
+
+int SumOfNumber(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+
+    return sum;
+}
+
+Console.WriteLine($"Сумма чисел числа {userNumber} равна {SumOfNumber(userNumber)}");
