d082bf0d7c3831d4415ff2deaa41add2c021043c
unknown <melpostov@yandex.rugit config --global user.name>
2023-09-02 16:07:19 +0300

    task20

--- task29/Program.cs
+++ task29/Program.cs
@@ -4,27 +4,35 @@
 1,2,5,7,19 -> [1,2,5,7,19]
 6,1,33 ->[6,1,33] */
 
-int[] GetRandomArray()
-{
-int[] array = new int[8];
-for (int i = 0; i < array.Length; i++)
-{
-    array[i] = new Random().Next(0,2);
+Console.WriteLine("Введите длинну массива ");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+int min = Convert.ToInt32(Console.ReadLine());
 
-}
-return array;
+int[] GetRandomArray(int arrayLength1, int start, int end)
+{
+    int[] array = new int[arrayLength1];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(start, end);
+    }
+    return array;
 }
 
-int[] userArray = GetRandomArray();
+int[] userArray = GetRandomArray(arrayLength, min, max);
 
 void PrintArray(int[] arrayToPrint)
 {
-    System.Console.Write("[ ");
+    Console.Write("[ ");
     for (int i = 0; i < arrayToPrint.Length; i++)
     {
-        System.Console.Write($"{arrayToPrint[i]} ");
+        Console.Write(arrayToPrint[i]);
+        if (i < arrayToPrint.Length - 1)
+        {
+            Console.Write(", ");
+        }
     }
-    System.Console.WriteLine("]");
+    Console.Write("]");
 }
 
 PrintArray(userArray);
