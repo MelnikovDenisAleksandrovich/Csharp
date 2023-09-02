991acf0e58e153f39e7d0f395de4aafbdc500304
unknown <melpostov@yandex.rugit config --global user.name>
2023-09-02 09:01:03 +0300

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите цикл,который принимает на вход 
+два числа (А и В) и возводит число А в 
+натуральную степень В.
+
+3,5 -> 243(3..5)
+2,4 -> 16
+*/
+
+Console.WriteLine("Введите числа A и B ");
+int numberA =Convert.ToInt32(Console.ReadLine());
+int numberB =Convert.ToInt32(Console.ReadLine());
+
+int ProductOfNumbers(int a, int b)
+{
+    int product = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        product = product*a;
+    }
+    return product;
+}
+
+Console.WriteLine($"число {numberA} в степени {numberB} равно {ProductOfNumbers(numberA,numberB)}");
