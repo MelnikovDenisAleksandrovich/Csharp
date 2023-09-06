7b2e468168aaf59f05703551a36e11f7cb8207f9
unknown <melpostov@yandex.rugit config --global user.name>
2023-09-07 02:35:20 +0300

    task45

---
+++ task45/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+45. Напишите програму, которая будет создавать копию заданного 
+одномерного массива с поэлемерного копирования.
+*/
+
+Console.Clear();
+
+int[] array = {1, 9, 3, 8, 5};
+
+int[] CopyArray(int[] array)
+{
+    int[] arrayL = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        arrayL[i] = array[i];
+    }
+    return arrayL;
+}
+
+void PrintArray(int[] arrayToPrint)
+
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write(arrayToPrint[i]);
+        if (i < arrayToPrint.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+
+int[] userArray = CopyArray(array);
+
+PrintArray(userArray);
