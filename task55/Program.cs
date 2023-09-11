2ee670c93724199942733373f9a7c3b6cde066c5
unknown <melpostov@yandex.rugit config --global user.name>
2023-09-11 17:25:41 +0300

    task55

---
+++ task55/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+55. Задайте двухмерный массив. Напишите программу, 
+которая заменит строки на столбцы. Вслучае, если это не возможно, программа  должна вывести сообщение для пользовотеля.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+В итоге получается вот такой массив:
+1 5 8 5
+4 9 4 2
+7 2 2 6
+2 3 4 7
+*/
+
+
+Console.Clear();
+
+int[,] GetRandom2DArray(int rowNumber, int max, int min)
+{
+    int[,] result = new int[rowNumber, rowNumber];
+    for (int i = 0; i < rowNumber; i++)
+    {
+        for (int j = 0; j < rowNumber; j++)
+        {
+            result[i, j] = new Random().Next (max, min) ;
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    System.Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            System.Console.Write(arrayToPrint[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] ChangeStringsAndColums(int[,] matrix)
+{
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = i; j < matrix.GetLength(1); j++)
+        {
+            temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+    return matrix;
+}
+
+int[,] userMatrix = GetRandom2DArray(5, 0, 10);
+Print2DArray(userMatrix);
+Print2DArray(ChangeStringsAndColums(userMatrix));
