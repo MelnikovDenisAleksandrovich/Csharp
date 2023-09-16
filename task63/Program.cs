294c6a596b16b65e17675476cda979d33a7ef892
unknown <melpostov@yandex.rugit config --global user.name>
2023-09-16 16:16:34 +0300

    task63 Рекурсия

---
+++ task63/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+63. Задайте значение N. Напишите программу, которая выдает все натуральные
+числа в промежутке от 1 до N.
+*/
+
+Console.Clear();
+
+void NuturalNum(int n, int start = 1)
+{
+    if (start <= n)
+    {
+        Console.Write(start + ", ");
+        NuturalNum(n, start + 1);
+    }
+}
+NuturalNum(5);
