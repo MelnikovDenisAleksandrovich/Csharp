6e89645cb8911abb82dca5ce1621ad567b8c9c78
unknown <melpostov@yandex.rugit config --global user.name>
2023-09-16 16:42:58 +0300

    task67 Рекурсия

---
+++ task67/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+67. Напишите программу, которая будет принимать на вход число и возращать сумму его цифр.
+*/ 
+
+Console.Clear();
+
+int result = SumNumber(256);
+
+int SumNumber(int n)
+{
+    if (n == 0)
+    return  0;
+    else
+    return n%10 + SumNumber(n/10); 
+}
+Console.WriteLine($"Сумма цифер числа N: {result}");
