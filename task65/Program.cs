1b44a731c5b1bdc3eb341eb7aa7c233c3e5cd737
unknown <melpostov@yandex.rugit config --global user.name>
2023-09-16 16:24:38 +0300

    task65 Рекурсия

---
+++ task65/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+65. Задайте значение M и N. Напишите программу, 
+которая выдает все натуральные числа в промежутке от  M до N.
+*/
+
+Console.Clear();
+
+void NuturalNum(int m, int n)
+{
+    if (m<= n)
+    {
+        Console.Write(m + ", ");
+        NuturalNum(m + 1, n);
+    }
+}
+NuturalNum(1,7);
